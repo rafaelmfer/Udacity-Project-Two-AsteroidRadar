b5543f4b03e8376fea1e3dc83211d796e0be146e
rafaelmfer <rafaelmfer@hotmail.com>
2021-06-21 01:47:29 -0300

    Created Data Access Object for Room

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
@@ -1,8 +1,23 @@
 package com.udacity.asteroidradar.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface AsteroidDatabaseDao {
 
+    @Query("SELECT * FROM asteroid_table")
+    fun getAllAsteroids(): LiveData<List<DatabaseAsteroid>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg asteroids: DatabaseAsteroid)
+
+    @Query("SELECT * FROM asteroid_table WHERE id = :idAsteroid")
+    fun getAsteroid(idAsteroid: Long): DatabaseAsteroid
+
+    @Query("DELETE FROM asteroid_table")
+    fun clear()
 }
