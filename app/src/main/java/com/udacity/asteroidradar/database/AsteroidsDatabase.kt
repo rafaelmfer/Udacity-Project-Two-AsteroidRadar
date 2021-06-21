8c2248bc5fdd918e877e161888813f3d7151a992
rafaelmfer <rafaelmfer@hotmail.com>
2021-06-21 01:49:51 -0300

    Put Entities on Room Database

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidsDatabase.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidsDatabase.kt
@@ -1,9 +1,11 @@
 package com.udacity.asteroidradar.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [DatabaseAsteroid::class], version = 1, exportSchema = false)
 abstract class AsteroidsDatabase : RoomDatabase() {
 
     abstract val asteroidDatabaseDao: AsteroidDatabaseDao
